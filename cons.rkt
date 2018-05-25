c1d5da8ae51518ea66cb5af51391755af9f80edc
unknown <linhchi.nguyen@soc-labo1-02.unitn.it>
2018-05-25 18:47:05 +0200

    details

--- cons.rkt
+++ cons.rkt
@@ -4,10 +4,10 @@
 (define SIM-ID 1)
 
 (define N 100)
-(define CYCLES 500)
+(define CYCLES 500000) ;; careful, you change the cycles here
 (define SPEED 10)
-(define ROUNDS 500000)
-(define DELTA .99)
+(define ROUNDS 500) ;; not here
+(define DELTA .50)
 (define DELTAstr (string-trim (number->string (* DELTA 100)) ".0"))
 (define MUTATION 1)
 
